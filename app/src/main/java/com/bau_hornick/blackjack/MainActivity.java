d85bfa36965b3f0495d184ee57c51df1425e0ebf
Thomas <tbau@my.apsu.edu>
2017-01-26 13:30:43 -0600

    Added back of cards and part of the layout of the game loop

--- app/src/main/java/com/bau_hornick/blackjack/MainActivity.java
+++ app/src/main/java/com/bau_hornick/blackjack/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        Intent intent=new Intent(this,DealerActivity.class);
+        Intent intent=new Intent(this,PlayActivity.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/bau_hornick/blackjack/PlayActivity.java
+++ app/src/main/java/com/bau_hornick/blackjack/PlayActivity.java
@@ -9,5 +9,7 @@ public class PlayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+
     }
 }
