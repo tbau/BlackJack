1fabfe7b96f241673e725343b4d180c2d6621435
Alex Hornick <tennalexd@yahoo.com>
2017-01-31 07:25:15 -0600

    Added listener to increase bet button.

--- app/src/main/java/com/bau_hornick/blackjack/PlayActivity.java
+++ app/src/main/java/com/bau_hornick/blackjack/PlayActivity.java
@@ -2,6 +2,9 @@ package com.bau_hornick.blackjack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class PlayActivity extends AppCompatActivity {
 
@@ -10,6 +13,16 @@ public class PlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
+        Button addBet = (Button) findViewById(R.id.increase_bet_button);
+        addBet.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+      else if (v.getId() == R.id.increase_bet_button) {
+            bet++;
+            TextView betView = (TextView) findViewById(R.id.bet_textView);
+            betView.setText(bet + "(" + (money - bet) + ")");
+        }
     }
 }
