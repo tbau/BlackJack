5c8dc0387edc4ad65632a9633e61e17a578e183b
Thomas <tbau@my.apsu.edu>
2017-01-31 07:42:55 -0600

    Added stuff to layout

--- app/src/main/java/com/bau_hornick/blackjack/PlayActivity.java
+++ app/src/main/java/com/bau_hornick/blackjack/PlayActivity.java
@@ -3,13 +3,13 @@ package com.bau_hornick.blackjack;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button
+import android.widget.Button;
 import android.widget.TextView;
 
-public class PlayActivity extends AppCompatActivity implements View.OnClickListener{
+public class PlayActivity extends AppCompatActivity implements View.OnClickListener {
 
     int bet;
-    int money=100;
+    int money = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,24 +18,22 @@ public class PlayActivity extends AppCompatActivity implements View.OnClickListe
 
         Button b = (Button) findViewById(R.id.increase_bet_button);
         b.setOnClickListener(this);
-     
+
         b = (Button) findViewById(R.id.decrease_bet_button);
         b.setOnClickListener(this);
     }
 
-  @Override
-  public void onClick(View v) {
-        if(v.getId()==R.id.decrease_bet_button){
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.decrease_bet_button) {
             bet--;
             TextView tv = (TextView) findViewById(R.id.bet_textView);
-            tv.setText(bet+"("+(money-bet)+")");
-        }
-  
-      else if (v.getId() == R.id.increase_bet_button) {
+            tv.setText(bet + "(" + (money - bet) + ")");
+        } else if (v.getId() == R.id.increase_bet_button) {
             bet++;
             TextView betView = (TextView) findViewById(R.id.bet_textView);
             betView.setText(bet + "(" + (money - bet) + ")");
         }
     }
-
+}
     
