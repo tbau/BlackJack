1d6bcbceb97857fc362d404f002fd700eecd3036
Alex Hornick <tennalexd@yahoo.com>
2017-01-30 21:11:45 -0600

    Added Card class, began Deck class

---
+++ app/src/main/java/com/bau_hornick/blackjack/Card.java
@@ -0,0 +1,26 @@
+package com.bau_hornick.blackjack;
+
+/**
+ * Created by tennkarene on 1/30/2017.
+ */
+
+public class Card {
+    private int image;
+    private int value;
+    private String suit;
+
+    public String getSuit()
+    {
+        return suit;
+    }
+
+    public int getValue()
+    {
+        return value;
+    }
+
+    public int getImage()
+    {
+        return image;
+    }
+}
---
+++ app/src/main/java/com/bau_hornick/blackjack/Deck.java
@@ -0,0 +1,20 @@
+package com.bau_hornick.blackjack;
+import java.util.ArrayList;
+
+/**
+ * Created by tennkarene on 1/30/2017.
+ */
+
+public class Deck {
+    private ArrayList<Card> deck; //vector of 52 cards
+
+    public void init() //create all 52 cards and store in deck. Store value, image, suit for each card.
+    {
+
+    }
+
+    public void shuffle() //shuffle deck
+    {
+
+    }
+}
