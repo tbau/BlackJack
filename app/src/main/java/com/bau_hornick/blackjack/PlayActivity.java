e703a95791c3f30ba4e8360e49a71962ba7290d5
Thomas <tbau@my.apsu.edu>
2017-02-07 07:28:06 -0600

    Fixed portrait layout for play screen

--- app/src/main/java/com/bau_hornick/blackjack/PlayActivity.java
+++ app/src/main/java/com/bau_hornick/blackjack/PlayActivity.java
@@ -38,14 +38,12 @@ public class PlayActivity extends AppCompatActivity implements View.OnClickListe
     ArrayList<ImageView> playerImages;
     ArrayList<ImageView> dealerImages;
 
-    int dealerImageViews[] = {R.id.dealer_card1_imageView, R.id.dealer_card2_imageView,
-            R.id.dealer_card3_imageView, R.id.dealer_card4_imageView,
-            R.id.dealer_card5_imageView, R.id.dealer_card6_imageView,
-            R.id.dealer_card7_imageView, R.id.dealer_card8_imageView};
-    int playerImageViews[] = {R.id.player_card1_imageView, R.id.player_card2_imageView,
-            R.id.player_card3_imageView,R.id.player_card4_imageView,
-            R.id.player_card5_imageView, R.id.player_card6_imageView, R.id.player_card7_imageView,
-            R.id.player_card8_imageView};
+    int dealerImageViews[] = {R.id.dealer_card1_imageView,R.id.dealer_card2_imageView,R.id.dealer_card3_imageView,
+            R.id.dealer_card4_imageView,R.id.dealer_card5_imageView,R.id.dealer_card6_imageView,R.id.dealer_card7_imageView,
+            R.id.dealer_card8_imageView,R.id.dealer_card8_imageView,R.id.dealer_card9_imageView,R.id.dealer_card10_imageView};
+    int playerImageViews[] = {R.id.player_card1_imageView,R.id.player_card2_imageView,R.id.player_card3_imageView,R.id.player_card4_imageView,
+            R.id.player_card5_imageView,R.id.player_card6_imageView,R.id.player_card7_imageView,R.id.player_card8_imageView,
+            R.id.player_card9_imageView,R.id.player_card10_imageView};
 
 
     enum STATE{BEFORE, PLAYER,DEALER};
@@ -162,9 +160,9 @@ public class PlayActivity extends AppCompatActivity implements View.OnClickListe
                 TextView tv = (TextView) findViewById(R.id.deck_count_textView);
                 tv.setText(String.valueOf(deck.getDeck().size()));
 
-                if (playerHand.getDeck().size() == 5 && playerScore < 21) {
+                if (playerHand.getDeck().size() == 10 && playerScore < 21) {
 
-                    Toast.makeText(getApplicationContext(), "You have reached the five card limit!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "You have reached the ten card limit!", Toast.LENGTH_SHORT).show();
 
                     Runnable r1 = new Runnable() {
                         @Override
@@ -338,7 +336,7 @@ public class PlayActivity extends AppCompatActivity implements View.OnClickListe
                            outputMessage(OutputContext.PLAYER,bet,STATE.BEFORE,"BEFORE");
                        }
                    }else{
-                       while(dealerScore<17){
+                       while(dealerScore<17&&dealerHand.getDeck().size()<10){
                            dealerHand.getDeck().add(deck.getDeck().get(deck.getDeck().size()-1));
                            deck.getDeck().remove(deck.getDeck().size()-1);
 
